fc439ddb8854098523232336d28a727522e7b5d6
yungyuc <yyc@seety.org>
2010-03-17 10:58:07 -0400

    Add runtime statistics anchor.

--- solvcon/anchor.py
+++ solvcon/anchor.py
@@ -27,8 +27,6 @@ class Anchor(object):
         pass
     def prefull(self):
         pass
-    def postfull(self):
-        pass
     def prehalf(self):
         pass
     def premarchsoln(self):
@@ -46,6 +44,8 @@ class Anchor(object):
     def prebcdsoln(self):
         pass
     def posthalf(self):
+        pass
+    def postfull(self):
         pass
     def postloop(self):
         pass
@@ -77,6 +77,46 @@ class AnchorList(list):
             runanchors = reversed(runanchors)
         for anchor in runanchors:
             getattr(anchor, method)()
+
+class RuntimeStatAnchor(Anchor):
+    """
+    Report the Linux load average through solver.  Reports are made after a
+    full marching interation.
+
+    @ivar reports: list what should be reported.  Default is ['loadavg'] only.
+    @itype reports: list
+    """
+    def __init__(self, svr, **kw):
+        self.reports = kw.pop('reports', ['loadavg'])
+        super(RuntimeStatAnchor, self).__init__(svr, **kw)
+
+    def _cpu(self):
+        """
+        Usually useless since while the method is executing, CPUs are mostly
+        idling.
+        """
+        names = ['us', 'sy', 'ni', 'id', 'wa', 'hi', 'si', 'st']
+        f = open('/proc/stat')
+        totcpu = f.readlines()[0]
+        f.close()
+        frame = [float(it) for it in totcpu.split()[1:]]
+        scale = [it/sum(frame)*100 for it in frame]
+        msg = 'cpu:'
+        for it in range(len(names)):
+            msg += ' %s%s' % ('%.2f%%'%scale[it], names[it])
+        return msg
+
+    def _loadavg(self):
+        f = open('/proc/loadavg')
+        loadavg = f.read()
+        f.close()
+        return 'loadavg: %s' % ' '.join(loadavg.split()[:3])
+
+    def postfull(self):
+        import sys
+        if not sys.platform.startswith('linux'): return
+        for rep in self.reports:
+            self.svr.mesg(getattr(self, '_'+rep)()+'\n')
 
 class ZeroIAnchor(Anchor):
     """
