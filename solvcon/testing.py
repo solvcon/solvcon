3c6512a2ffff0b57fb0cc8fb280c7c18f7e3f601
yungyuc <yyc@seety.org>
2010-11-11 22:38:26 -0500

    Add email.

--- solvcon/testing.py
+++ solvcon/testing.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 #
-# Copyright (C) 2008-2010 Yung-Yu Chen.
+# Copyright (C) 2008-2010 Yung-Yu Chen <yyc@solvcon.net>.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
