2b1f8d2f15b2cf9feac3417e71dd2ade8e47362d
yungyuc <yyc@seety.org>
2011-01-23 18:22:31 -0500

    Use temporary directories instead of hard-coded directory name.

--- solvcon/tests/test_batch.py
+++ solvcon/tests/test_batch.py
@@ -85,60 +85,71 @@ echo "Finish @`date`."'''
         from nose.plugins.skip import SkipTest
         if sys.platform.startswith('win'): raise SkipTest
         import os, shutil
+        from tempfile import mkdtemp
         from ..case import BlockCase
         from ..batch import batregy
-        case = BlockCase(rootdir='/tmp')
+        wdir = mkdtemp()
+        case = BlockCase(rootdir=wdir)
         msg = []
         case.info = lambda m: msg.append(m)
         sbm = batregy.Torque(case, arnname='arn')
-        if not os.path.exists('/tmp/arn'):
-            os.makedirs('/tmp/arn')
+        if not os.path.exists(os.path.join(wdir, 'arn')):
+            os.makedirs(os.path.join(wdir, 'arn'))
         fnlist = sbm.tofile()
         for it in range(len(fnlist)):
-            self.assertEqual(fnlist[it], '/tmp/arn/arn.pbs%d'%it)
+            self.assertEqual(fnlist[it], os.path.join(wdir, 'arn',
+                'arn.pbs%d'%it))
         self.assertEqual(len(msg), 1)
-        self.assertEqual(msg[0], 'Job directory was there: /tmp/arn\n')
-        shutil.rmtree('/tmp/arn')
-        self.assertFalse(os.path.exists('/tmp/arn'))
+        self.assertEqual(msg[0], 'Job directory was there: %s\n' %
+            os.path.join(wdir, 'arn'))
+        shutil.rmtree(wdir)
+        self.assertFalse(os.path.exists(wdir))
 
     def test_without_jobdir(self):
         import sys
         from nose.plugins.skip import SkipTest
         if sys.platform.startswith('win'): raise SkipTest
         import os, shutil
+        from tempfile import mkdtemp
         from ..case import BlockCase
         from ..batch import batregy
-        case = BlockCase(rootdir='/tmp')
+        wdir = mkdtemp()
+        case = BlockCase(rootdir=wdir)
         msg = []
         case.info = lambda m: msg.append(m)
         sbm = batregy.Torque(case, arnname='arn')
-        if os.path.exists('/tmp/arn'):
-            shutil.rmtree('/tmp/arn')
+        if os.path.exists(os.path.join(wdir, 'arn')):
+            shutil.rmtree(os.path.join(wdir, 'arn'))
         fnlist = sbm.tofile()
         for it in range(len(fnlist)):
-            self.assertEqual(fnlist[it], '/tmp/arn/arn.pbs%d'%it)
+            self.assertEqual(fnlist[it], os.path.join(wdir, 'arn',
+                'arn.pbs%d'%it))
         self.assertEqual(len(msg), 0)
-        shutil.rmtree('/tmp/arn')
-        self.assertFalse(os.path.exists('/tmp/arn'))
+        shutil.rmtree(wdir)
+        self.assertFalse(os.path.exists(wdir))
 
     def test_with_empty(self):
         import sys
         from nose.plugins.skip import SkipTest
         if sys.platform.startswith('win'): raise SkipTest
         import os, shutil
+        from tempfile import mkdtemp
         from ..case import BlockCase
         from ..batch import batregy
-        case = BlockCase(rootdir='/tmp', empty_jobdir=True)
+        wdir = mkdtemp()
+        case = BlockCase(rootdir=wdir, empty_jobdir=True)
         msg = []
         case.info = lambda m: msg.append(m)
         sbm = batregy.Torque(case, arnname='arn')
-        if not os.path.exists('/tmp/arn'):
-            os.makedirs('/tmp/arn')
+        if not os.path.exists(os.path.join(wdir, 'arn')):
+            os.makedirs(os.path.join(wdir, 'arn'))
         fnlist = sbm.tofile()
         for it in range(len(fnlist)):
-            self.assertEqual(fnlist[it], '/tmp/arn/arn.pbs%d'%it)
+            self.assertEqual(fnlist[it], os.path.join(wdir, 'arn',
+                'arn.pbs%d'%it))
         self.assertEqual(len(msg), 2)
-        self.assertEqual(msg[0], 'Job directory was there: /tmp/arn\n')
+        self.assertEqual(msg[0], 'Job directory was there: %s\n' % (
+            os.path.join(wdir, 'arn'),))
         self.assertEqual(msg[1], 'Delete all file in job directory.\n')
-        shutil.rmtree('/tmp/arn')
-        self.assertFalse(os.path.exists('/tmp/arn'))
+        shutil.rmtree(wdir)
+        self.assertFalse(os.path.exists(wdir))
