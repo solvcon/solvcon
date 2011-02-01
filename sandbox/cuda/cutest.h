daa19d60bb1743a96cbe944d4f9cacd95923b388
yungyuc <yyc@seety.org>
2011-02-01 14:47:44 -0500

    Organize source in directory.

---
+++ sandbox/cuda/cutest.h
@@ -0,0 +1,6 @@
+#ifndef SOLVCON_CUTEST_H
+#define SOLVCON_CUTEST_H
+
+__global__ void VecAdd(float* A, float* B, float* C, int N);
+
+#endif
--- sandbox/cuda/test.cu
+++ sandbox/cuda/test.cu
@@ -1,11 +1,5 @@
 #include <stdio.h>
-
-// Device code
-__global__ void VecAdd(float* A, float* B, float* C, int N) {
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-    if (i < N)
-        C[i] = A[i] + B[i];
-};
+#include "cutest.h"
 
 // Host code
 int main(int argc, char **argv) {
