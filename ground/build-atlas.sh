139f988d2e4e356b1bac3f08b9870985d42dc6bc
yungyuc <yyc@solvcon.net>
2011-05-20 15:00:05 -0400

    Build atlas.

---
+++ ground/build-atlas.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LAPACK=`pwd`/../lapack-3.3.1/lapack_LINUX.a
+ATLAS_PLAT=ATLAS_LINUX
+
+# determine arch.
+if [ `uname -m` == 'x86_64' ]; then
+  BITS='-b 64'
+else
+  BITS='-b 32'
+fi
+
+mkdir -p $ATLAS_PLAT
+cd $ATLAS_PLAT
+../configure --prefix=$SCPREFIX \
+	-Si cputhrchk 0 \
+	$BITS \
+	-Fa alg -fPIC \
+	--with-netlib-lapack=$LAPACK \
+> configure.log 2>&1
+
+make > make.log 2>&1
+make install > install.log 2>&1
--- ground/generate-numpy.py
+++ ground/generate-numpy.py
@@ -1,34 +1,22 @@
 #!/usr/bin/env python
 
 site_tmpl = """[DEFAULT]
-library_dirs = /usr/local/lib:%(BUILD)s/ATLAS/ATLAS_LINUX/lib:%(BUILD)s/umfpack:%(OPT)s/fftw3/lib
-include_dirs = /usr/local/include:%(BUILD)s/ATLAS/ATLAS_LINUX/include:%(BUILD)s/umfpack:%(OPT)s/fftw3/include
+library_dirs = /usr/local/lib:%(SCPREFIX)s/lib
+include_dirs =
+/usr/local/include:%(SCPREFIX)s/include:%(SCPREFIX)s/include/atlas
 
 [atlas]
-atlas_libs = lapack, f77blas, cblas, atlas
-
-[amd]
-amd_libs = amd
-
-[umfpack]
-umfpack_libs = umfpack
-
-[fftw]
-# fftw no longer supported in scipy 0.7.
-libraries = fftw3"""
+atlas_libs = lapack, f77blas, cblas, atlas"""
 
 setup_tmpl = """[config_fc]
-fcompiler = gnu95
+fcompiler = gfortran
 """
 
 import os
 
 def main():
     f = open("site.cfg", 'w')
-    f.write(site_tmpl % dict(
-        BUILD=os.environ.get('HOME', '')+'/build/lib',
-        OPT=os.environ.get('HOME', '')+'/opt',
-    ))
+    f.write(site_tmpl % os.environ)
     f.close()
 
     f = open("setup.cfg", 'w')
