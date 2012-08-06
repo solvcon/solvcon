4a71d9d6e1838c36218e754734723e21a20a27db
yungyuc <yyc@solvcon.net>
2012-08-06 19:25:11 +0800

    Add dependency check from pyx files to pxd files.

--- site_scons/site_tools/cython.py
+++ site_scons/site_tools/cython.py
@@ -16,8 +16,17 @@ AUTHORS:
  - Dag Sverre Seljebotn
 
 """
+import os
+import re
+try:
+    import set
+except ImportError:
+    from sets import Set as set
+import itertools
+
 import SCons
 from SCons.Builder import Builder
+from SCons.Scanner import Scanner
 from SCons.Action import Action
 
 #def cython_action(target, source, env):
@@ -43,6 +52,31 @@ def create_builder(env):
 def cython_suffix_emitter(env, source):
     return "$CYTHONCFILESUFFIX"
 
+def cython_scan(node, env, path, arg=None):
+    """
+    A simple .pyx scanner for one-line cimports of .pxd files.
+    """
+    # scan file to extract all possible cimports.
+    contents = node.get_text_contents()
+    names = [reo.findall(contents) for reo in [
+        re.compile(r'^\s*from\s+(.+?)\s+cimport\s.*$', re.M),
+        re.compile(r'^\s*cimport\s+(.+?)$', re.M),
+    ]]
+    names = itertools.chain(*names)
+    # keep characters before " as ".
+    names = [name.split(' as ')[0] for name in names]
+    # split each cimport.
+    names = itertools.chain(*[name.split(',') for name in names])
+    names = [name.strip() for name in names]
+    # remove duplications.
+    names = set(names)
+    # prepend with the directory of the original pyx file.
+    prefix = os.path.dirname(env.GetBuildPath(node))
+    names = [os.path.join(prefix, '%s.pxd'%name) for name in names]
+    # only take local pxd file and ignore anything unfound.
+    names = [name for name in names if os.path.exists(name)]
+    return [env.File(name) for name in names]
+
 def generate(env):
     env["CYTHON"] = "cython"
     env["CYTHONCOM"] = "$CYTHON $CYTHONFLAGS -o $TARGET $SOURCE"
@@ -57,6 +91,9 @@ def generate(env):
     c_file.add_action('.py', cythonAction)
 
     create_builder(env)
+
+    pyxscanner = Scanner(function=cython_scan, skeys=['.pyx'], name='PYX')
+    env.Append(SCANNERS=[pyxscanner])
 
 def exists(env):
     try:
