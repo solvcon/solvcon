cd89a5d6cae188d9cc6003e0c6f8aa3895cd1737
yungyuc <yyc@solvcon.net>
2013-03-18 18:01:06 +0800

    Back out previous change.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -97,10 +97,7 @@ modindex_common_prefix = ['solvcon.']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-if os.environ.get('READTHEDOCS', None) == 'True':
-    html_theme = '/default.css'
-else:
-    html_theme = 'default'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
