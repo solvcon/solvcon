120b486f74610d0778fa2b6663e2b26ffc26a41b
yungyuc <yyc@solvcon.net>
2013-03-18 17:26:09 +0800

    Instruct RTD to use my theme.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -97,7 +97,10 @@ modindex_common_prefix = ['solvcon.']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+if os.environ.get('READTHEDOCS', None) == 'True':
+    html_theme = '/default.css'
+else:
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
