709e1bf1b68d0c11897614b810559b8e4596e3ca
yungyuc <yyc@solvcon.net>
2013-08-04 23:51:27 +0800

    Also verify distutils on drone.io.

--- contrib/droneio/standard.sh
+++ contrib/droneio/standard.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 # install dependency.
 contrib/aptget.ubuntu.12.04LTS.sh
-# build
+# build with scons.
 scons
+# unit tests.
+nosetests -v
+# functional tests.
+nosetests ftests/* --exclude test_rpc --exclude test_remote -v
+# remove generated binaries.
+find solvcon -name *.so -delete
+rm -rf build/ lib/
+# rebuild with distutils.
+python setup.py build_ext --inplace
 # unit tests.
 nosetests -v
 # functional tests.
