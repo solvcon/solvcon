761341ffacdb13ae9ca3fea5a49b2707c674e4ee
Yung-Yu Chen <yyc@solvcon.net>
2014-05-05 22:16:10 +0800

    Add Mercurial to conda.sh.

--- contrib/conda.sh
+++ contrib/conda.sh
@@ -1,5 +1,5 @@
 conda install -c https://conda.binstar.org/collections/yungyuc/solvcon \
-  setuptools \
+  setuptools mercurial \
   scons cython numpy netcdf4 scotch nose gmsh vtk sphinx
 lret=$?; if [[ $lret != 0 ]] ; then exit $lret; fi
 easy_install -UZ sphinxcontrib-issuetracker
