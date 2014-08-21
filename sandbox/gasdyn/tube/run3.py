71a4acbc605853381564cf67ab6bbd4cfbd88bd3
Taihsiang Ho (tai271828) <taihsiangho@gmail.com>
2014-08-22 00:02:15 +0800

    merge sodtube1dmtx.py into sodtube1d.py

--- sandbox/gasdyn/tube/run3.py
+++ sandbox/gasdyn/tube/run3.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
+
 import sodtube1d
+
 sodtube = sodtube1d.SodTube()
 sodtube.gen_mesh()
 mesh = sodtube.get_mesh()
-solution = sodtube.cal_analytic_Solution(mesh)
+solution_analytic = sodtube.cal_analytic_Solution(mesh)
 
+solution_cese = sodtube.get_cese_Solution()
 
+dm = sodtube1d.DataManager()
+dm.dump_solution(solution_analytic)
+dm.dump_solution(solution_cese)
