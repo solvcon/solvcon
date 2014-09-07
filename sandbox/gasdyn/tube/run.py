133235ec9724404fab2530d56ed34f93e27c338f
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2014-09-08 00:05:32 +0800

    implement ways to get l2 norm in different mesh size. (but the result value is not as what we expected. sth. must be wrong)

--- sandbox/gasdyn/tube/run.py
+++ sandbox/gasdyn/tube/run.py
@@ -27,9 +27,28 @@ dm = sodtube1d.DataManager()
 #dm.dump_solution(solution_cese)
 #dm.dump_solution(solution_deviation)
 #dm.dump_solution(solution_deviation_percent)
-dm.get_plot_solutions_fig_rho(solution_analytic, solution_cese, "analytic rho", "cese rho")
-dm.get_plot_solutions_fig_v(solution_analytic, solution_cese, "analytic v", "cese v")
-dm.get_plot_solutions_fig_p(solution_analytic, solution_cese, "analytic p", "cese p")
-#dm.show_solution_comparison()
+
+#dm.get_plot_solutions_fig_rho(solution_analytic, solution_cese, "analytic rho", "cese rho")
+#dm.get_plot_solutions_fig_v(solution_analytic, solution_cese, "analytic v", "cese v")
+#dm.get_plot_solutions_fig_p(solution_analytic, solution_cese, "analytic p", "cese p")
 print("l2 norm is: (rho, v, p)")
-print(dm.get_l2_norm(solution_analytic, solution_cese))
+#print(dm.get_l2_norm(solution_analytic, solution_cese,1),
+#      dm.get_l2_norm(solution_analytic, solution_cese,2),
+#      dm.get_l2_norm(solution_analytic, solution_cese,3))
+print(dm.get_l2_norm(solution_analytic, solution_cese,1,[(-0.25,-0.15),(-0.05,0.05),(0.15,0.25),(0.30,0.40)]),
+      dm.get_l2_norm(solution_analytic, solution_cese,2,[(-0.25,-0.15),(0.30,0.40)]),
+      dm.get_l2_norm(solution_analytic, solution_cese,3,[(-0.25,-0.15),(-0.05,0.05),(0.30,0.40)]))
+
+solution_cese = sodtube.get_cese_solution(200, 0.002)
+#dm.get_plot_solutions_fig_rho(solution_analytic, solution_cese, "analytic rho (diff mesh)", "cese rho")
+#dm.get_plot_solutions_fig_v(solution_analytic, solution_cese, "analytic v (diff mesh)", "cese v")
+#dm.get_plot_solutions_fig_p(solution_analytic, solution_cese, "analytic p (diff mesh)", "cese p")
+#dm.show_solution_comparison()
+print(dm.get_l2_norm(solution_analytic, solution_cese,1,[(-0.25,-0.15),(-0.05,0.05),(0.15,0.25),(0.30,0.40)]),
+      dm.get_l2_norm(solution_analytic, solution_cese,2,[(-0.25,-0.15),(0.30,0.40)]),
+      dm.get_l2_norm(solution_analytic, solution_cese,3,[(-0.25,-0.15),(-0.05,0.05),(0.30,0.40)]))
+
+solution_cese = sodtube.get_cese_solution(400, 0.001)
+print(dm.get_l2_norm(solution_analytic, solution_cese,1,[(-0.25,-0.15),(-0.05,0.05),(0.15,0.25),(0.30,0.40)]),
+      dm.get_l2_norm(solution_analytic, solution_cese,2,[(-0.25,-0.15),(0.30,0.40)]),
+      dm.get_l2_norm(solution_analytic, solution_cese,3,[(-0.25,-0.15),(-0.05,0.05),(0.30,0.40)]))
