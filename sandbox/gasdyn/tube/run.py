7fddc413a7700b9285a57cbf878c228b61da6535
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2014-11-02 22:50:13 +0800

    use appropriate iteration number to make the grids are the same

--- sandbox/gasdyn/tube/run.py
+++ sandbox/gasdyn/tube/run.py
@@ -31,7 +31,7 @@ dm = sodtube1d.DataManager()
 #dm.get_plot_solutions_fig_rho(solution_analytic, solution_cese_fp, "analytic rho", "cese rho")
 #dm.get_plot_solutions_fig_v(solution_analytic, solution_cese_fp, "analytic v", "cese v")
 #dm.get_plot_solutions_fig_p(solution_analytic, solution_cese_fp, "analytic p", "cese p")
-#print("l2 norm is: (rho, v, p)")
+print("l2 norm is: (rho, v, p)")
 #print(dm.get_l2_norm(solution_analytic, solution_cese_fp,1),
 #      dm.get_l2_norm(solution_analytic, solution_cese_fp,2),
 #      dm.get_l2_norm(solution_analytic, solution_cese_fp,3))
@@ -39,12 +39,14 @@ print(dm.get_l2_norm(solution_analytic, solution_cese_fp,1,[(-0.25,-0.15),(-0.05
       dm.get_l2_norm(solution_analytic, solution_cese_fp,2,[(-0.25,-0.15),(0.30,0.40)]),
       dm.get_l2_norm(solution_analytic, solution_cese_fp,3,[(-0.25,-0.15),(-0.05,0.05),(0.30,0.40)]))
 
-sodtube.gen_mesh(50, -5025, 5025)
+sodtube.gen_mesh(50, -5050, 5050)
 mesh = sodtube.get_mesh()
 solution_analytic = sodtube.get_analytic_solution(mesh)
-solution_cese_fp = sodtube.get_cese_solution_fortran_porting(200, 0.002, 0.005)
+solution_cese_fp = sodtube.get_cese_solution_fortran_porting(201, 0.002, 0.005)
+#dm.dump_solution(solution_analytic)
+#dm.dump_solution(solution_cese_fp)
 
-dm.get_plot_solutions_fig_rho(solution_analytic, solution_cese_fp, "analytic rho (diff mesh)", "cese rho")
+#dm.get_plot_solutions_fig_rho(solution_analytic, solution_cese_fp, "analytic rho (diff mesh)", "cese rho")
 #dm.get_plot_solutions_fig_v(solution_analytic, solution_cese_fp, "analytic v (diff mesh)", "cese v")
 #dm.get_plot_solutions_fig_p(solution_analytic, solution_cese_fp, "analytic p (diff mesh)", "cese p")
 #dm.show_solution_comparison()
@@ -52,7 +54,11 @@ print(dm.get_l2_norm(solution_analytic, solution_cese_fp,1,[(-0.25,-0.15),(-0.05
       dm.get_l2_norm(solution_analytic, solution_cese_fp,2,[(-0.25,-0.15),(0.30,0.40)]),
       dm.get_l2_norm(solution_analytic, solution_cese_fp,3,[(-0.25,-0.15),(-0.05,0.05),(0.30,0.40)]))
 
-solution_cese_fp = sodtube.get_cese_solution_fortran_porting(400, 0.001, 0.0025)
+sodtube.gen_mesh(25, -5050, 5050)
+mesh = sodtube.get_mesh()
+solution_analytic = sodtube.get_analytic_solution(mesh)
+solution_cese_fp = sodtube.get_cese_solution_fortran_porting(403, 0.001, 0.0025)
+#dm.dump_solution(solution_cese_fp)
 print(dm.get_l2_norm(solution_analytic, solution_cese_fp,1,[(-0.25,-0.15),(-0.05,0.05),(0.15,0.25),(0.30,0.40)]),
       dm.get_l2_norm(solution_analytic, solution_cese_fp,2,[(-0.25,-0.15),(0.30,0.40)]),
       dm.get_l2_norm(solution_analytic, solution_cese_fp,3,[(-0.25,-0.15),(-0.05,0.05),(0.30,0.40)]))
