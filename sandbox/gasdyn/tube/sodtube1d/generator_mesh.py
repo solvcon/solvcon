92e32dbe15f06b80ad88431d8d0ad76cd4d5e54b
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2014-12-06 01:19:06 +0800

    implement demo ipython notebook prototype for 1D sod tube. build how the physical model looks like.

--- sandbox/gasdyn/tube/sodtube1d/generator_mesh.py
+++ sandbox/gasdyn/tube/sodtube1d/generator_mesh.py
@@ -6,7 +6,7 @@
 #   a generator to help you to have a mesh.
 #
 
-from matplotlib.pyplot import scatter
+import matplotlib.pyplot as plt
 
 class Mesher():
     def __init__(self):
@@ -31,4 +31,33 @@ class Mesher():
         because this method is used image inline ipython notebook.
         """
         list_values_y = [0]*len(self.mesh)
-        scatter(self.mesh, list_values_y)
+        plt.scatter(self.mesh, list_values_y)
+
+    def show_mesh_physical_model(self):
+        """
+        Show how 1D Sod tube may look like.
+        TODO:
+            1. provide a bar for users to zoom in and out
+            2. indicate the location of the diaphragm
+        """
+        from mpl_toolkits.mplot3d import axes3d
+        import numpy as np
+
+        fig = plt.figure()
+        ax = axes3d.Axes3D(fig,azim=30,elev=30)
+
+        x = np.linspace(-1,1,10)
+        y = np.linspace(-1,1,10)
+
+        X, Y = np.meshgrid(x,y)
+        Z = np.sqrt(1-X**2)
+
+        ax.plot_wireframe(X,Y,Z)
+        ax.plot_wireframe(X,Y,-Z)
+
+        ax.set_xbound(lower=-10.0, upper=10.0)
+        ax.set_zbound(lower=-10.0, upper=10.0)
+        #ax.set_axis_off()
+        ax.set_xlabel('x-axis')
+        ax.set_ylabel('y-axis')
+        ax.set_zlabel('z-axis')
