428aa9dcfe575231bbcaa9f92fa12f424b00b153
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2014-12-05 22:46:09 +0800

    refactoring the naming mtx to be mtx_

--- sandbox/gasdyn/tube/sodtube1d/solver_cese.py
+++ sandbox/gasdyn/tube/sodtube1d/solver_cese.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 #
-# sodtube1d.py
+# solver_cese.py
 #
 # Description:
-#     1D Sod Tube Test
+#     1D Sod Tube sover based on CESE method.
 #
 #     This program is implemented by OO style to be
 #     a part of ipython notebook demo materials.
@@ -12,12 +12,9 @@
 #     is based on the book,
 #     Principles of Computational Fluid Dynamics,
 #     written by Pieter Wesseling.
-#     Or, someone could refer to the solvcon website
+#     Or, people could refer to the solvcon website
 #     http://www.solvcon.net/en/latest/cese.html#sod-s-shock-tube-problem
 #
-#
-# DEBUG: search string 'DEBUG'
-# why: somewhere don't understand very much ...
 
 import numpy as np
 import generator_mesh
@@ -99,20 +96,20 @@ class Solver():
         # this also means the status of the gas
         # on grids.
         # prefix mtx_ means 'matrix of sth.'
-        mtxq = np.asmatrix(np.zeros(shape=(3, mesh_pt_number_x)))
+        mtx_q = np.asmatrix(np.zeros(shape=(3, mesh_pt_number_x)))
         # u_m, but means 'next' u_m
         # u_m at the next time step 
-        mtxqn = np.asmatrix(np.zeros(shape=(3, mesh_pt_number_x)))
+        mtx_qn = np.asmatrix(np.zeros(shape=(3, mesh_pt_number_x)))
         # partial matrix of q partial x
-        mtxqx = np.asmatrix(np.zeros(shape=(3, mesh_pt_number_x)))
+        mtx_qx = np.asmatrix(np.zeros(shape=(3, mesh_pt_number_x)))
         # partial mastrix of q partial t
-        mtxqt = np.asmatrix(np.zeros(shape=(3, mesh_pt_number_x)))
+        mtx_qt = np.asmatrix(np.zeros(shape=(3, mesh_pt_number_x)))
         # matrix s, it is a part of the marching mtx_qn
-        mtxs = np.asmatrix(np.zeros(shape=(3, mesh_pt_number_x)))
+        mtx_s = np.asmatrix(np.zeros(shape=(3, mesh_pt_number_x)))
         vxl = np.zeros(shape=(3,1))
         vxr = np.zeros(shape=(3,1))
 
-        mtxf = np.asmatrix(np.zeros(shape=(3,3)))
+        mtx_f = np.asmatrix(np.zeros(shape=(3,3)))
         
         hdt = dt/2.0
         qdt = dt/4.0 #q:quad
@@ -122,27 +119,27 @@ class Solver():
         tt = hdt*it
         dtx = dt/dx
         
-        mtxq[0][0] = rhol
-        mtxq[1][0] = rhol*ul
-        mtxq[2][0] = pl/a1 + 0.5*rhol*ul**2.0
+        mtx_q[0][0] = rhol
+        mtx_q[1][0] = rhol*ul
+        mtx_q[2][0] = pl/a1 + 0.5*rhol*ul**2.0
         mesh_pt_number_x_at_half_t = it + 1
         # initialize the gas status before the diaphragm
         # was removed.
         for i in xrange(mesh_pt_number_x_at_half_t):
-            mtxq[0,i+1] = rhor
-            mtxq[1,i+1] = rhor*ur
-            mtxq[2,i+1] = pr/a1 + 0.5*rhor*ur**2.0
+            mtx_q[0,i+1] = rhor
+            mtx_q[1,i+1] = rhor*ur
+            mtx_q[2,i+1] = pr/a1 + 0.5*rhor*ur**2.0
 
         # m is the number used to calculate the status before
         # the half delta t stepping is applied.
         m = 2 # move out from the diaphragm which the 0th grid.
         for i in xrange(it):
-            self.get_cese_status_before_half_dt(m, mtxq, mtxf, mtxqt, mtxqx, mtxs)
+            self.get_cese_status_before_half_dt(m, mtx_q, mtx_f, mtx_qt, mtx_qx, mtx_s)
             # stepping into the next halt delta t
             # m mesh points along t could introduce m - 1 mesh points along t + 0.5*dt
-            self.get_cese_status_after_half_dt(m, mtxq, mtxqn, mtxqt, mtxqx, mtxs)
+            self.get_cese_status_after_half_dt(m, mtx_q, mtx_qn, mtx_qt, mtx_qx, mtx_s)
             #  ask the status at t + 0.5*dt to be the next status before the half delta t is applied
-            m = self.push_status_along_t(m, mtxq, mtxqn)
+            m = self.push_status_along_t(m, mtx_q, mtx_qn)
             
         #solution__x_start_index = mesh_x.get_start_grid(mesh_pt_number_x)
         solution_x_start_index = 50
@@ -150,9 +147,9 @@ class Solver():
         for i in range(mesh_pt_number_x):
             solution_x = mesh_x[i + solution_x_start_index]
             x = solution_x
-            solution_rho = mtxq[0,i]
-            solution_v = mtxq[1,i]/mtxq[0,i]
-            solution_p = a1*(mtxq[2,i] - 0.5*(solution_v**2)*mtxq[0,i])
+            solution_rho = mtx_q[0,i]
+            solution_v = mtx_q[1,i]/mtx_q[0,i]
+            solution_p = a1*(mtx_q[2,i] - 0.5*(solution_v**2)*mtx_q[0,i])
             solution.append((solution_x, solution_rho, solution_v, solution_p))
         
         return solution
@@ -181,33 +178,33 @@ class Solver():
     def cal_cese_solution(self, initcondition, mesh, ceseparameters):
         return self.solution
 
-    def get_cese_status_before_half_dt(self, m, mtxq, mtxf, mtxqt, mtxqx, mtxs):
+    def get_cese_status_before_half_dt(self, m, mtx_q, mtx_f, mtx_qt, mtx_qx, mtx_s):
         """
         the status is 
         """
         for j in xrange(m):
-            w2 = mtxq[1,j]/mtxq[0,j]
-            w3 = mtxq[2,j]/mtxq[0,j]
+            w2 = mtx_q[1,j]/mtx_q[0,j]
+            w3 = mtx_q[2,j]/mtx_q[0,j]
             # f[0][0] = 0.0
-            mtxf[0,1] = 1.0
+            mtx_f[0,1] = 1.0
             # f[0][2] = 0.0
-            mtxf[1,0] = -a3*w2**2
-            mtxf[1,1] = a2*w2
-            mtxf[1,2] = ga - 1.0
-            mtxf[2,0] = a1*w2**3 - ga*w2*w3
-            mtxf[2,1] = ga*w3 - a1*w2**2
-            mtxf[2,2] = ga*w2
+            mtx_f[1,0] = -a3*w2**2
+            mtx_f[1,1] = a2*w2
+            mtx_f[1,2] = ga - 1.0
+            mtx_f[2,0] = a1*w2**3 - ga*w2*w3
+            mtx_f[2,1] = ga*w3 - a1*w2**2
+            mtx_f[2,2] = ga*w2
 
             # (4.17) in chang95
-            mtxqt[:,j] = -1.0*mtxf*mtxqx[:,j]
+            mtx_qt[:,j] = -1.0*mtx_f*mtx_qx[:,j]
             # (4.25) in chang95
             # the n_(fmt)_j of the last term should be substitubed
             # by the other terms.
-            mtxs[:,j] = qdx*mtxqx[:,j] + dtx*mtxf*mtxq[:,j] \
-                        - dtx*qdt*mtxf*mtxf*mtxqx[:,j]
-        return  m, mtxq, mtxf, mtxqt, mtxqx, mtxs
+            mtx_s[:,j] = qdx*mtx_qx[:,j] + dtx*mtx_f*mtx_q[:,j] \
+                        - dtx*qdt*mtx_f*mtx_f*mtx_qx[:,j]
+        return  m, mtx_q, mtx_f, mtx_qt, mtx_qx, mtx_s
 
-    def get_cese_status_after_half_dt(self, m, mtxq, mtxqn, mtxqt, mtxqx, mtxs):
+    def get_cese_status_after_half_dt(self, m, mtx_q, mtx_qn, mtx_qt, mtx_qx, mtx_s):
         mm = m - 1
         for j in xrange(mm):
             # (4.24) in chang95
@@ -215,24 +212,24 @@ class Solver():
             # different t (n) from the j/j+1 on the right hand side,
             # which address the other t (n-1/2) on the space-time
             # surface.
-            mtxqn[:,j+1] = 0.5*(mtxq[:,j] \
-                                + mtxq[:,j+1] \
-                                + mtxs[:,j] - mtxs[:,j+1])
+            mtx_qn[:,j+1] = 0.5*(mtx_q[:,j] \
+                                + mtx_q[:,j+1] \
+                                + mtx_s[:,j] - mtx_s[:,j+1])
             # (4.27) and (4.36) in chang95
-            vxl = np.asarray((mtxqn[:,j+1] \
-                              - mtxq[:,j] - hdt*mtxqt[:,j]) \
+            vxl = np.asarray((mtx_qn[:,j+1] \
+                              - mtx_q[:,j] - hdt*mtx_qt[:,j]) \
                               /hdx)
-            vxr = np.asarray((mtxq[:,j+1] + hdt*mtxqt[:,j+1] \
-                              - mtxqn[:,j+1]) \
+            vxr = np.asarray((mtx_q[:,j+1] + hdt*mtx_qt[:,j+1] \
+                              - mtx_qn[:,j+1]) \
                               /hdx)
             # (4.39) in chang95
-            mtxqx[:,j+1] = np.asmatrix((vxl*((abs(vxr))**ia) \
+            mtx_qx[:,j+1] = np.asmatrix((vxl*((abs(vxr))**ia) \
                                         + vxr*((abs(vxl))**ia)) \
                                         /(((abs(vxl))**ia) \
                                             + ((abs(vxr))**ia) + 1.0E-60))
         
-    def push_status_along_t(self, number_mesh_points_before_hdt, mtxq, mtxqn):
+    def push_status_along_t(self, number_mesh_points_before_hdt, mtx_q, mtx_qn):
         for j in xrange(1,number_mesh_points_before_hdt):
-            mtxq[:,j] = mtxqn[:,j]
+            mtx_q[:,j] = mtx_qn[:,j]
         number_mesh_points_before_hdt_next_iter = number_mesh_points_before_hdt + 1
         return number_mesh_points_before_hdt_next_iter
