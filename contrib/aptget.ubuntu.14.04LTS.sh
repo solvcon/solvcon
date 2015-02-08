d02d76beef17948b8a4e16bcd7b5e180073c1919
Yung-Yu Chen <yyc@solvcon.net>
2015-02-08 21:48:01 +0800

    Ref issue #114 Add python-paramiko and python-boto to Ubuntu 14.04LTS install script
    Resolve #114

--- contrib/aptget.ubuntu.14.04LTS.sh
+++ contrib/aptget.ubuntu.14.04LTS.sh
@@ -3,5 +3,6 @@ sudo apt-get install build-essential gcc gfortran scons \
 liblapack-pic liblapack-dev libnetcdf-dev libnetcdfc7 netcdf-bin \
 libscotch-dev libscotchmetis-dev libscotch-5.1 \
 python2.7 python2.7-dev cython python-numpy python-nose gmsh python-vtk \
+python-paramiko python-boto \
 python-pygraphviz python-sphinx python-sphinxcontrib.issuetracker \
 mercurial dvipng texlive-math-extra texlive-latex-extra
