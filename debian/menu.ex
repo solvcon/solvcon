82f6143646e17afa9cb2d950aa998cfb88a17626
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2015-04-05 21:05:54 +0800

    Ref issue #119 remove useless automatically generated debian files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(solvcon):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="solvcon" command="/usr/bin/solvcon"
--- debian/solvcon.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the solvcon package
-#
-0 4	* * *	root	[ -x /usr/bin/solvcon_maintenance ] && /usr/bin/solvcon_maintenance
