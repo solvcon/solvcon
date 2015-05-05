651cdcd5fc9b2f447236d0b44729b550926b9474
Yung-Yu Chen <yyc@solvcon.net>
2015-05-05 19:40:02 +0800

    Update copyright year.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -67,7 +67,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SOLVCON'
-copyright = u'2009-2014, SOLVCON Project'
+copyright = u'2009-2015, SOLVCON Project'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
