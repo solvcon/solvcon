84f23dd0f0250cde620fd69e50fe3f8dfb4d98af
Yung-Yu Chen <yyc@solvcon.net>
2016-05-15 07:55:14 +0800

    Ref issue #153 Create a place for SOLVCON C++ code.
    Create a new directory "libmarch/" to house the C++ code, organized as a
    header-only C++ library "libmarch", which is responsible for
    time-marching for the spatial loops.  libmarch requires at least a C++11
    compiler to work.  Eventually, all spatial loops should be ported from C
    to C++.

---
+++ libmarch/include/march/march.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+/*
+ * Copyright (c) 2016, Yung-Yu Chen <yyc@solvcon.net>
+ * BSD 3-Clause License, see COPYING
+ */
+
+#include "march/core/core.hpp"
+#include "march/mesh/mesh.hpp"
+
+// vim: set ff=unix fenc=utf8 nobomb et sw=4 ts=4:
