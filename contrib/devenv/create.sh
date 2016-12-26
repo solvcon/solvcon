cae263c2f4132306691fe99a6ad3bb22ffeb09f6
Yung-Yu Chen <yyc@solvcon.net>
2016-12-26 21:22:46 +0800

    Fix conda Python version to 3.5 before 3.6 stabilizes

--- contrib/devenv/create.sh
+++ contrib/devenv/create.sh
@@ -8,5 +8,10 @@ fi
 SCDEVENV_DST=${SCDEVENV_SRC}/../../build/${SCDEVENV_DST}
 echo "Create environment at ${SCDEVENV_DST}"
 mkdir -p ${SCDEVENV_DST}
-conda create -p ${SCDEVENV_DST}/install --no-default-packages -y python
+if [ "3" == "`python -c 'import sys; sys.stdout.write(str(sys.version_info.major))'`" ] ; then
+  # Fix version to 3.5 before conda Python 3.6 stabilizes
+  conda create -p ${SCDEVENV_DST}/install --no-default-packages -y python=3.5
+else
+  conda create -p ${SCDEVENV_DST}/install --no-default-packages -y python
+fi
 cp -f ${SCDEVENV_SRC}/start ${SCDEVENV_DST}/start
