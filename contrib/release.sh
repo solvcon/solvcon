32f6688a36ce48ac31d0b02d682281425d5cbda8
Yung-Yu Chen <yyc@solvcon.net>
2017-11-01 18:57:14 +0800

    Reduce travis ci run time.
    Remove in-repository setup.py and optimized libmarch builds.

--- contrib/release.sh
+++ contrib/release.sh
@@ -1,17 +1,31 @@
-#!/bin/sh
+#!/bin/bash -x
+
 SCVER=`python -c 'import sys; import solvcon; sys.stdout.write("%s\\n" % solvcon.__version__)'`
-# build source distribution file.
-rm -rf dist/SOLVCON-${SCVAR}*
+
+# package source distribution file.
+rm -rf dist/SOLVCON-${SCVER}*
 python setup.py clean
 python setup.py sdist
-# build the project.
+
+# unpack the distribution file.
 cd dist
 rm -rf SOLVCON-${SCVER}/
 tar xfz SOLVCON-${SCVER}.tar.gz
 cd SOLVCON-${SCVER}
+
+retval=0
+
+# build.
 python setup.py build_ext --inplace
+lret=$?; if [[ $lret != 0 ]] ; then retval=$lret ; fi
+
 # test.
 PYTHONPATH=`pwd`
 nosetests --with-doctest
+lret=$?; if [[ $lret != 0 ]] ; then retval=$lret ; fi
 nosetests ftests/gasplus/* -v
+lret=$?; if [[ $lret != 0 ]] ; then retval=$lret ; fi
 nosetests ftests/parallel/*
+lret=$?; if [[ $lret != 0 ]] ; then retval=$lret ; fi
+
+exit $retval
