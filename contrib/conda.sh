ccdff5b4226e979999ab104df431bafc496b6446
Taihsiang Ho (tai271828) <e@tai271828.me>
2021-10-12 08:56:27 +0200

    ci(github): run CI with Python 3.8
    Issue: #238

--- contrib/conda.sh
+++ contrib/conda.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 conda install -y \
-  python=3.6 \
+  python=3.8 \
   cmake setuptools pip sphinx ipython jupyter \
   cython numpy hdf4 netcdf4 nose pytest paramiko boto graphviz
 lret=$?; if [[ $lret != 0 ]] ; then exit $lret; fi
-conda install -y -c https://conda.anaconda.org/yungyuc \
-  gmsh scotch
+conda install -y -c https://conda.anaconda.org/yungyuc scotch
 lret=$?; if [[ $lret != 0 ]] ; then exit $lret; fi
 pip install pybind11 pythreejs
 lret=$?; if [[ $lret != 0 ]] ; then exit $lret; fi
