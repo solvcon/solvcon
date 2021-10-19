0addc6c9bafeeea2ffa5599201d28b03ccc51d50
Taihsiang Ho (tai271828) <e@tai271828.me>
2021-10-19 13:43:51 +0200

    ci(github): migrate packages from conda to devenv
    Issue: #242

--- contrib/conda.sh
+++ contrib/conda.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 conda install -y \
   python=3.8 \
-  cmake setuptools pip sphinx ipython jupyter \
-  cython numpy hdf4 netcdf4 nose pytest paramiko boto graphviz
+  cmake \
+  numpy hdf4 netcdf4 nose boto paramiko
 lret=$?; if [[ $lret != 0 ]] ; then exit $lret; fi
 conda install -y -c https://conda.anaconda.org/yungyuc scotch
-lret=$?; if [[ $lret != 0 ]] ; then exit $lret; fi
-pip install pybind11 pythreejs
 lret=$?; if [[ $lret != 0 ]] ; then exit $lret; fi
