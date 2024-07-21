d8aaaaf0a376f9fd8034b358bdac86416c4ddca0
Yung-Yu Chen <yyc@solvcon.net>
2024-07-21 21:11:31 +0800

    Update issuetracker Sphinx extension: Sphinx.info()
    `Sphinx.info()` is deprecated.  Removing it to prevent the following error:
    
    AttributeError: 'Sphinx' object has no attribute 'info'

--- doc/ext/issuetracker/__init__.py
+++ doc/ext/issuetracker/__init__.py
@@ -302,12 +302,10 @@ def init_transformer(app):
 def copy_stylesheet(app, exception):
     if app.builder.name != 'html' or exception:
         return
-    app.info(bold('Copying issuetracker stylesheet... '), nonl=True)
     dest = path.join(app.builder.outdir, '_static', 'issuetracker.css')
     source = path.join(path.abspath(path.dirname(__file__)),
                        'issuetracker.css')
     copyfile(source, dest)
-    app.info('done')
 
 
 def setup(app):
