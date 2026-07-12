c781389ce5526beef51a1485dd96d5aa9cdea18b
arturict <arturict@users.noreply.github.com>
2026-07-12 13:16:24 +0200

    Remove unused Ubuntu dependency scripts

--- contrib/dependency/apt.ubuntu-1804-xenial.sh
+++
@@ -1,4 +0,0 @@
-# Install compiler and tools.
-apt-get -qy install sudo build-essential make cmake libc6-dev gcc-7 g++-7 curl git
-# Install Python.
-apt-get -qy install python3 cython3 python3-numpy python3-nose python3-pytest
