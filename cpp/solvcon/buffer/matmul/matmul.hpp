1aff3ad7044c19efa403dd49de2b64b1f798b193
ThreeMonth03 <austin20463@gmail.com>
2026-08-20 15:36:09 +0800

    Split `matmul` implementation by responsibility

---
+++ cpp/solvcon/buffer/matmul/matmul.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+/*
+ * Copyright (c) 2026, solvcon team <contact@solvcon.net>
+ * BSD 3-Clause License, see COPYING
+ */
+
+#include <solvcon/buffer/matmul/executor.hpp>
+#include <solvcon/buffer/matmul/plan.hpp>
+
+// vim: set ff=unix fenc=utf8 et sw=4 ts=4 sts=4:
